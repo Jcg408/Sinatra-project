a5ec1a10e262353126f50aa1e9b14201c08ed9b2
Jcg408 <jcg321@comcas.net>
2019-03-11 11:36:10 -0400

    work on view interactions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,19 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+
     erb :index
   end
 
   helpers do
-    def current_user
-      User.find(session[:user_id])
-    end
-
     def logged_in?
       !!session[:user_id]
     end
+
+    def current_user
+     User.find(session[:user_id])
+    end
+
   end
 
 
